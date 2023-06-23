ef7d103d8a58311eefd3265d67630266a98efe3d
ArtemEgorov-user <136822678+ArtemEgorov-user@users.noreply.github.com>
2023-06-24 02:08:57 +0300

    Delete Lesson 1.py

--- Lesson 1.py
+++
@@ -1,7 +0,0 @@
-def is_pollindrom(string):
-    return string.strip() == string[::1].strip()
-
-if __name__ == "__main__":
-    user_word = input()
-    result = is_pollindrom(user_word)
-    print(result)
