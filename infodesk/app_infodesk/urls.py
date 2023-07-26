90ae110993db1b2ca92c317604b2e19b6965102e
ArtemEgorov-user <artemvegorov@yandex.ru>
2023-07-26 17:19:04 +0300

    Commit to the changes/

--- infodesk/app_infodesk/urls.py
+++ infodesk/app_infodesk/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import index, top_sellers
+from .views import index, top_sellers, advertisement, advertisement_post, login, register, profile
 
 urlpatterns = [
-    path("", index),
-    path("top-sellers/", top_sellers)
+    path("", index, name="main_page"),
+    path("top-sellers/", top_sellers, name="top_sellers"),
+    path("advertisement-post/", advertisement_post, name="advertisement-post"),
+    path("advertisement/", advertisement, name="advertisement"),
+    path("login/", login, name="login"),
+    path("profile/", profile, name="profile"),
+    path("register/", register, name="register"),
 ]
